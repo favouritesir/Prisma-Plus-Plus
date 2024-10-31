961419340a04b07942201e0bfdfd4c73736ea204
ashik <gub221902131@gmail.com>
2024-10-31 15:47:26 +0600

    initialize setup & environment

---
+++ Packeges/src/PPP@V1/ppp.script.v1.ts
@@ -0,0 +1,9 @@
+// ============================================================================================
+// ===================================== PRISMA PLUS PLUS =====================================
+// ============================================================================================
+// AUTHOR       : Ashikur Rahman
+// DESCRIPTION  : Prisma++ is an utility library for Prisma ORM to provide a smooth experience
+//                and to increase code readability. It is free software thats makes Prisma easy.
+// VERSION      : v1.0.0
+// Date         : Thursday, 31 -October-2024 (15:40:08)
+// ============================================================================================
